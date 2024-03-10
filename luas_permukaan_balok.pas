5e1fd227fbe7f1a5b9c913af7198b66f1fc81c98
FeruziakintaSyakira <162893345+FeruziakintaSyakira@users.noreply.github.com>
2024-03-10 20:52:41 +0700

    Create luas_permukaan_balok.pas

---
+++ luas_permukaan_balok.pas
@@ -0,0 +1,24 @@
+program luas_permukaan_balok;
+var
+  L,T,P,luas:real;
+uses crt;
+begin
+  clrscr;
+  writeln('Nama  : Feruziakinta Syakira');
+  Writeln('NIM   : 2310431008');
+  writeln('Shift : 1');
+  writeln('');
+  
+  //L=lebar//
+  //P=panjang//
+  //T=tinggi//
+  write('Lebar   = ');
+  readln(L);
+  write('Tinggi  = ');
+  readln(T);
+  write('Panjang = ');
+  readln(P);
+  luas:= 2*((p*l)+(p*t)+(l*t));
+  writeln('Luas    = ',luas);
+  readln;
+end.
