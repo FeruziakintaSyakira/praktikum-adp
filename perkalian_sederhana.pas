3cc20f30c10883f711406aedd63c9c4332da8f8f
FeruziakintaSyakira <162893345+FeruziakintaSyakira@users.noreply.github.com>
2024-03-10 20:47:57 +0700

    Create perkalian_sederhana.pas

---
+++ perkalian_sederhana.pas
@@ -0,0 +1,18 @@
+program perkalian_sederhana;
+var
+   nilai_a,nilai_b,hasil:real;
+uses crt;
+begin
+  clrscr;
+  writeln('Nama : Feruziakinta Syakira');
+  Writeln('NIM  : 2310431008');
+  writeln('Shift: 1');
+  writeln('');
+  write('Nilai a = ');
+  readln(nilai_a);
+  write('Nilai b = ');
+  readln(nilai_b);
+  hasil:=nilai_a*nilai_b;
+  writeln('Hasil   = ',hasil);
+  readln;
+end.
