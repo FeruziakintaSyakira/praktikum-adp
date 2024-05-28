3e2bb3b931436b702b1cade50f5e1a95346e2f43
FeruziakintaSyakira <162893345+FeruziakintaSyakira@users.noreply.github.com>
2024-05-28 16:29:15 +0700

    Create rumah.py

---
+++ rumah.py
@@ -0,0 +1,44 @@
+from termcolor import colored, cprint
+
+cprint("        _______","blue")
+cprint("       /\                    \ ","blue")
+cprint("      /  \________\ ","blue")
+cprint("     /    \                    \ ","blue")
+cprint("    /      \________\ ","blue")
+cprint("   /        \                    \ ","blue")
+cprint("  /          \________\ ","blue")
+cprint(" /            \                    \ ","blue")
+cprint("/              \                    \ ","blue")
+cprint("           HOME SWEET HOME           ","white","on_red")
+for i in range(3):
+	cprint(" "*37,"white","on_red")
+for i in range(1):
+	cprint(" "*9,"white","on_red",end="")
+	cprint(" "*2,"white","on_black",end="")
+	cprint(" "*1,"white","on_red",end="")
+	cprint(" "*2,"white","on_black",end="")
+	cprint(" "*9,"white","on_red",end="")
+	cprint(" "*2,"white","on_black",end="")
+	cprint(" "*1,"white","on_red",end="")
+	cprint(" "*2,"white","on_black",end="")
+	cprint(" "*9,"white","on_red")
+for i in range (1):
+	cprint(" "*37,"white","on_red")
+for i in range(1):
+	cprint(" "*9,"white","on_red",end="")
+	cprint(" "*2,"white","on_black",end="")
+	cprint(" "*1,"white","on_red",end="")
+	cprint(" "*2,"white","on_black",end="")
+	cprint(" "*9,"white","on_red",end="")
+	cprint(" "*2,"white","on_black",end="")
+	cprint(" "*1,"white","on_red",end="")
+	cprint(" "*2,"white","on_black",end="")
+	cprint(" "*9,"white","on_red")
+for i in range (5):
+	cprint(" "*37,"white","on_red")
+for i in range(4):
+	cprint(" "*15,"white","on_red",end="")
+	cprint(" "*7,"white","on_black",end="")
+	cprint(" "*15,"white","on_red")
+for i in range(1):
+	cprint(" "*37,"white","on_red")
