8cd3b9afc11f4812ee2fcdde04d40616c7036ad5
FeruziakintaSyakira <162893345+FeruziakintaSyakira@users.noreply.github.com>
2024-05-28 16:28:29 +0700

    Create bendera.py

---
+++ bendera.py
@@ -0,0 +1,8 @@
+from termcolor import colored, cprint
+
+for i in range (4):
+	cprint(" "*30,"white","on_red")
+for j in range (4):
+	cprint(" "*30, "white","on_white")
+for i in range(15):
+	cprint(" "*3,"blue","on_dark_grey")
